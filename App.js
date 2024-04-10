260dd75db6ef43b04ad222ba0ab8fdd5c7d09ced
joaoghg <jherreiragarnica@gmail.com>
2024-04-09 21:20:15 -0300

    removendo imports inúteis

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from "./src/navigation/StackNavigator";
 
 export default function App() {
   return (
     <>
+      <StatusBar style={"auto"} />
       <StackNavigator />
     </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
