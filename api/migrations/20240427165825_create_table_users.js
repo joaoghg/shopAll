98294349d72b35d38bef09db09b8da8b417f6c2b
joaoghg <jherreiragarnica@gmail.com>
2024-04-27 14:05:01 -0300

    migration users

---
+++ api/migrations/20240427165825_create_table_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.string('email').notNullable().unique()
+    table.string('password').notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
+};
