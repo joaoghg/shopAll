e52ecaf4a856b476aeb498be671590b77488bc10
joaoghg <joao@esw.net.br>
2024-05-19 15:20:39 -0300

    migrations

--- api/migrations/20240519181543_create_table_product_images.js
+++ api/migrations/20240519181543_create_table_product_images.js
@@ -4,24 +4,24 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('product_images', table => {
-      table.increments('id').primary()
-      table.integer('productId').unsigned().notNullable()
-      table.string('path')
-      table.boolean('default').defaultTo(false)
-      table.foreign('productId').references('products.id')
+        table.increments('id').primary()
+        table.integer('productId').unsigned().notNullable()
+        table.string('path')
+        table.boolean('default').defaultTo(false)
+        table.foreign('productId').references('products.id')
     })
-  };
+};
   
-  /**
-   * @param { import("knex").Knex } knex
-   * @returns { Knex.SchemaBuilder }
-   */
-  exports.down = function(knex) {
-    return knex.schema.alterTable('product_images', function(table) {
-        table.dropForeign('productId')
-    })
-    .then(function() {
-        return knex.schema.dropTable('product_images')
-    });
-  };
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Knex.SchemaBuilder }
+ */
+exports.down = function(knex) {
+return knex.schema.alterTable('product_images', function(table) {
+    table.dropForeign('productId')
+})
+.then(function() {
+    return knex.schema.dropTable('product_images')
+});
+};
   
