9b7ea0232956bff4765654759e743293a09269d0
joaoghg <jherreiragarnica@gmail.com>
2024-05-19 23:34:24 -0300

    Auth

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -4,7 +4,7 @@ export const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
 
-  const server = "http://192.168.15.50:8000"
+  const server = "http://192.168.0.117:8000"
 
   return (
     <AuthContext.Provider value={{ server }}>
