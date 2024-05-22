dfda89957d27e21d5ccb319cb23ef724d4c9f9f7
joaoghg <jherreiragarnica@gmail.com>
2024-05-22 00:20:02 -0300

    Senha do banco no .env

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   client: 'pg',
   connection: {
     database: 'shop_all',
     user:     'postgres',
-    password: 'Jg4951393830'
+    password: process.env.DATABASE_PASS
   },
   migrations: {
     tableName: 'knex_migrations'
