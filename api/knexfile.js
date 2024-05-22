5f3b8fa450451021918674de893532cd890b9e3a
joaoghg <jherreiragarnica@gmail.com>
2024-05-22 00:30:06 -0300

    Tentando conexão

--- api/knexfile.js
+++ api/knexfile.js
@@ -3,8 +3,10 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
+    host: 'postgres://joaoghg:ZxyQ0xSijUotAKXCX9rdTumz6YwcNLbI@dpg-cp6m9dmv3ddc73fmn1tg-a.oregon-postgres.render.com/shop_all',
+    port: 5432,
     database: 'shop_all',
-    user:     'postgres',
+    user:     'joaoghg',
     password: process.env.DATABASE_PASS
   },
   migrations: {
