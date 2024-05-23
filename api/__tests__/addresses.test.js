b8da5de8fc605db0acc9778b25a9f33b95ba33c8
joaoghg <joao@esw.net.br>
2024-05-23 10:31:57 -0300

    testes

--- api/__tests__/addresses.test.js
+++ api/__tests__/addresses.test.js
@@ -7,6 +7,11 @@ require('dotenv').config();
 
 const secretKey = process.env.TEST_SECRET_KEY
 
+app.use((req, res, next) => {
+    req.secretKey = secretKey
+    next()
+})
+
 describe("addresses", () => {
     it("sem token", async () => {
         const response = await request.post('/addresses')
@@ -26,5 +31,42 @@ describe("addresses", () => {
             .set('Authorization', 'jkaksjaksjdksaskdjskal')
 
         expect(response.status).toBe(403)
+    })
+    it("Adicionando endereço válido", async () => {
+        const hashedPassword = await bcrypt.hash('12345678', 10);
+        const user = await db('users')
+        .returning('id')
+        .insert({
+            name: 'Teste',
+            email: 'test@address.com',
+            password: hashedPassword,
+            verificationToken: 'token'
+        });
+
+        const userId = user[0].id
+
+        const ret = await request.post('/login')
+          .send({
+            email: 'test@address.com',
+            password: '12345678'
+          });
+    
+        const token = ret._body.token
+
+        const response = await request.post('/addresses')
+            .send({
+                name : 'teste',
+                mobileNumber: '123456789',
+                houseNumber: null,
+                street: 'rua',
+                landmark: 'complemento',
+                cep: '17280000',
+                userId: userId,
+                city: 'cidade',
+                country: 'pais',
+                state: 'estado',
+                neighborhood: 'centro'
+            })
+            .set('Authorization', token)
     })
 })
